adfe91c2540347e2e4bc359b21fdfe231882b279
Banjo Tola <banjotola12@gmail.com>
2022-01-29 20:47:34 +0100

    adding a default date for task

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -1,48 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import notify from "../Notify/Notify";
 
 const Addtask = ({ onAdd, tasks }) => {
-
+  const [todaysDate, setTodaysDate] = useState("")
   const [taskObj, setTaskObj] = useState({
     text: "",
     day: "",
     time: "",
-    reminder: false
+    reminder: false,
   });
+
 
   const handleChange = (ev) => {
     let value = ev.target.value;
     const newid = Math.floor(Math.random() * 200000) + 1;
     // const newTask = {id:id, ...taskObj}
     setTaskObj({ ...taskObj, id: newid, [ev.target.name]: value });
+    console.log("date", taskObj.day);
+    // console.log("date", taskObj.day);
   };
 
   const changeRminder = (ev) => {
-    ev.target.checked ? (
-      setTaskObj({ ...taskObj, reminder: true })
-     ) : (
-      setTaskObj({ ...taskObj, reminder: false })
-     )
+    ev.target.checked
+      ? setTaskObj({ ...taskObj, reminder: true })
+      : setTaskObj({ ...taskObj, reminder: false });
   };
 
   const onSubmit = (ev) => {
     ev.preventDefault();
-    if (!taskObj.text || !taskObj.day || !taskObj.time) {
+    if (!taskObj.text || !taskObj.time) {
       notify("kindly fill the input fileds", "warn");
       return;
-    } 
-
-
-
+    }
+    console.log("date date", taskObj.day);
     onAdd(taskObj);
 
     setTaskObj({
       text: "",
-      day: "",
+      day: todaysDate,
       time: "",
+      reminder: false
     });
-
   };
+
+  const getDate = () => {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = `${yyyy}-${mm}-${dd}`
+
+    setTaskObj({...taskObj, day: today})
+    setTodaysDate(today)
+  };
+
+  useEffect(() => {
+    getDate();
+  }, []);
 
   return (
     <form className="add-form" onSubmit={onSubmit}>
@@ -54,6 +69,7 @@ const Addtask = ({ onAdd, tasks }) => {
           type="text"
           placeholder="add a task"
           value={taskObj.text}
+   
           onChange={handleChange}
         />
       </div>
@@ -63,9 +79,11 @@ const Addtask = ({ onAdd, tasks }) => {
         <input
           name="day"
           type="date"
-          value={taskObj.day}
+          // value={todaysDate}
+          value={taskObj.day }
           onChange={handleChange}
         />
+
       </div>
       <div className="form-control">
         <label className="label">Time</label>
